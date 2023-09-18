2da4d2045144b310cbf480e8aa0d5c9c1b0f7687
Yuliya Karuk <karuk.yuliya@gmail.com>
2023-09-19 00:44:32 +0400

    feat: add function for change tracks, handle audio time; change music and create songs.json

--- audio-player/src/js/audio.js
+++ audio-player/src/js/audio.js
@@ -1,3 +1,4 @@
+import songsJSON from '../data/songs.json' assert { type: 'json' }
 export { AudioPlayer };
 
 const AudioPlayerClasses = {
@@ -12,9 +13,9 @@ class AudioPlayer {
         this.buttonMute = document.querySelector('.controls__volume_mute');
         this.buttonPrevious = document.querySelector('.controls__previous');
         this.buttonNext = document.querySelector('.controls__next');
-        this.controlTimer = document.querySelector('.timer__input');
         this.currentTimeElement = document.querySelector('.timer_time_current');
         this.durationTimeElement = document.querySelector('.timer_time_duration');
+        this.controlTimer = document.querySelector('.timer__input');
 
         this.songTitle = document.querySelector('.song__title');
         this.songSinger = document.querySelector('.song__singer');
@@ -23,6 +24,8 @@ class AudioPlayer {
 
         this.audio;
         this.isPlayed = false;
+        this.singId = 1;
+        this.song;
 
         this.bindListeners();
     }
@@ -31,34 +34,101 @@ class AudioPlayer {
         const context = this;
 
         document.addEventListener("DOMContentLoaded", () => context.createAudio());
-
         this.buttonPlay.addEventListener('click', () => context.playAudio());
         this.buttonPause.addEventListener('click', () => context.pauseAudio());
+        this.buttonPrevious.addEventListener('click', () => context.changeAudio(-1));
+        this.buttonNext.addEventListener('click', () => context.changeAudio(1));
 
     }
 
     createAudio() {
         this.audio = new Audio();
+        this.fillAudio();
+        this.listenAudioTime();
+    }
+
+    fillAudio() {
+        this.findSong(this.singId);
+        this.audio.src = this.song.src;
+        this.audio.currentTime = 0;
+        this.songTitle.innerHTML = this.song.title;
+        this.songSinger.innerHTML = this.song.singer;
+        this.albumImage.src = this.song.img;
+        this.body.style.backgroundImage = `url(${this.song.img})`;
+        this.audio.onloadeddata = () => {
+            this.showTime();
+        }
+    }
+
+    findSong(id) {
+        this.song = songsJSON[id];
     }
 
     playAudio() {
-        this.audio.src = `src/audio/beyonce.mp3`;
-        this.audio.currentTime = 0;
         this.audio.play();
         this.isPlayed = true;
-        this.toggleButtonPlay();
+        this.fillButtonPlay();
     }
 
     pauseAudio() {
         this.audio.pause();
         this.isPlayed = false;
-        this.toggleButtonPlay();
+        this.fillButtonPlay();
     }
 
-    toggleButtonPlay() {
-        this.buttonPlay.classList.toggle(AudioPlayerClasses.buttonHide);
-        this.buttonPause.classList.toggle(AudioPlayerClasses.buttonHide);
+    fillButtonPlay() {
+        if (this.isPlayed) {
+            this.buttonPlay.classList.add(AudioPlayerClasses.buttonHide);
+            this.buttonPause.classList.remove(AudioPlayerClasses.buttonHide);
+        }
+        if (!this.isPlayed) {
+            this.buttonPlay.classList.remove(AudioPlayerClasses.buttonHide);
+            this.buttonPause.classList.add(AudioPlayerClasses.buttonHide);
+        }
     }
 
+    changeAudio(value) {
+        this.singId = this.singId + value;
+        if (this.singId === songsJSON.length ) {
+            this.singId = 0;
+        }
+        if (this.singId === -1 ) {
+            this.singId = 4;
+        }
+        this.fillAudio();
+        this.playAudio();
+    }
 
+    listenAudioTime() {
+        const context = this;
+        this.audio.addEventListener('timeupdate', () => context.handleAudioTime());
+        this.audio.addEventListener('ended', () => context.changeAudio(1));
+        this.controlTimer.addEventListener('input', () => context.moveTimer());
+    }
+
+    handleAudioTime() {
+        this.showTime();
+        this.showTimer();
+    }
+
+    showTime() {
+        const currentMinutes = Math.floor(this.audio.currentTime / 60);
+        const currentSeconds = Math.floor(this.audio.currentTime - currentMinutes * 60);
+        const durationMinutes = Math.floor(this.audio.duration / 60);
+        const durationSeconds = Math.floor(this.audio.duration - durationMinutes * 60);
+
+        this.currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0' + currentSeconds : currentSeconds}`;
+        this.durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds < 10 ? '0' + durationSeconds : durationSeconds}`;
+    }
+
+    showTimer() {
+        let percent = Math.floor((this.audio.currentTime / this.audio.duration) * 100);
+        this.controlTimer.style.background = `linear-gradient(to right, #B01C25 0%, #B01C25 ${percent}%, #999999 ${percent}%, #999999 100%)`;
+        this.controlTimer.value = percent;
+    }
+
+    moveTimer() {
+        const progress = this.controlTimer.value * this.audio.duration / 100;
+        this.audio.currentTime = progress;
+    }
 }
