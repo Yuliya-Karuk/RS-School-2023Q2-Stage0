80124e887fa37f6167a38b0fc637b7e0b740f13d
Yuliya Karuk <karuk.yuliya@gmail.com>
2023-10-02 15:34:12 +0400

    feat: add function for update position

--- random-game/src/js/game.js
+++ random-game/src/js/game.js
@@ -15,13 +15,29 @@ class Game {
         this.width = this.canvas.width
         this.height = this.canvas.height;
         this.player = new Player(this)
+        this.keys = []
 
-        this.render();
+        this.updatePlayer();
+        // window.addEventListener('keydown', (e) => this.player.updatePlayerLocation(e.key))
+        window.addEventListener('keydown', (e) => {
+            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+        })
+        window.addEventListener('keyup', (e) => {
+            const index = this.keys.indexOf(e.key);
+            if (index > -1) this.keys.splice(index, 1);
+        })
     }
 
     render() {
-        console.log(this.width, this.height);
         this.player.drawPlayer(this.context);
+        this.player.updatePlayerLocation();
+    }
 
+    updatePlayer() {
+        const ctx = this;
+        ctx.context.clearRect(0, 0, ctx.width, ctx.height);
+        ctx.render();
+        
+        // window.requestAnimationFrame(() => ctx.updatePlayer());
     }
 }
--- random-game/src/js/player.js
+++ random-game/src/js/player.js
@@ -7,9 +7,18 @@ class Player {
         this.height = 100;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
+        this.speed = 5;
     }
 
     drawPlayer(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    updatePlayerLocation() {
+        // if (movement === 'ArrowLeft') this.x -= this.speed;
+        // if (movement === 'ArrowRight') this.x += this.speed;
+        if(this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+        if(this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+
     }
 }
